531aad88b6a730e6a381622f694953a32a5d1a2e
Nick Volpe <nick@iamnickvolpe.com>
2015-05-03 00:44:06 -0700

    Added back in abs url

--- src/components/permalink/controllers/index.js
+++ src/components/permalink/controllers/index.js
@@ -6,10 +6,7 @@ module.exports = function($scope,$location) {
         emoji: location.emoji || '🌝',
         quote: location.quote || 'Quote',
         bg: location.bg || 'https://s-media-cache-ak0.pinimg.com/736x/cc/d1/5d/ccd15de92b4755e52dd18736739a8e24.jpg',
-<<<<<<< HEAD
-        song: location.song || 'http://www.youtube.com/v/vRY9Ybi5zqI'
-=======
+        song: location.song || 'http://www.youtube.com/v/vRY9Ybi5zqI',
         url: absUrl
->>>>>>> 063588b2a3e9e68844c8bc417c45b1038945ef49
     }
 }
