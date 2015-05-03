314fd59f4def030388bf9da461efbcc035c880a5
Nick Volpe <nick@iamnickvolpe.com>
2015-05-02 18:53:21 -0700

    First Wizard Commit

--- src/components/wizard/controllers/index.js
+++ src/components/wizard/controllers/index.js
@@ -1,3 +1,33 @@
 module.exports = function($scope) {
-    $scope.message = 'WYYYYYZZZZERD'; 
+    $scope.message = 'THIS IS THE WIZARD';
+    $scope.wizardSection = 0;
+    $scope.selections = [{selectionNumber:null},{selectionNumber:null},{selectionNumber:null}];
+    
+    $scope.backgrounds = [
+    {
+        backgroundNumber:0,
+        backgroundUrl:'http://ak-hdl.buzzfed.com/static/2014-12/24/13/enhanced/webdr06/anigif_enhanced-buzz-22472-1419446000-10.gif'
+        },
+    {
+        backgroundNumber:1,
+        backgroundUrl:'http://stylesaveus.com/wp-content/uploads/2013/10/Style-Save-Us-National-Cat-Day-Uber-ASPCA.jpg'
+        },
+    {
+        backgroundNumber:2,
+        backgroundUrl:'http://nmah.com/files/2015/01/kittens.jpg'
+        }
+    ];
+    
+    $scope.sectionChange = function(direction) {
+        if(direction==='next') {
+            $scope.wizardSection++;
+        } else
+        if (direction==='previous') {
+            $scope.wizardSection--;
+        }
+    }
+    
+    $scope.choose = function(section, number) {
+        $scope.selections[section].selectionNumber = number;
+    }
 }
